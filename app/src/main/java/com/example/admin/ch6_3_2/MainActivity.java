1c43f79f009977c7f15044ac8c05d7ef8c36b0a4
MLR-SUN <sun.ios06444@gmail.com>
2018-04-02 17:52:05 +0800

    1. 宣告imagebutton的變數

--- app/src/main/java/com/example/admin/ch6_3_2/MainActivity.java
+++ app/src/main/java/com/example/admin/ch6_3_2/MainActivity.java
@@ -2,9 +2,10 @@ package com.example.admin.ch6_3_2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ImageButton imgBtn1,imgBtn2,imgBtn3; //宣告imagebutton變數
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
